bf371191b7abbc7fe0d2e36a3f311cc61602cbf4
Christopher Lopes <christopher.lopes@gmail.com>
2018-08-17 07:58:04 -0700

    Add other calculations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
     hours: number;
     minutes: number;
     seconds: number;
+    timeMinutes: number;
+    timeSeconds: number;
     selectedDistanceUnit = 'miles';
     distances: string[] = ['marathon', 'half-marathon', '10k', '5k'];
     distanceSuggestions: any[];
@@ -20,7 +22,39 @@ export class AppComponent {
     private previousHours: number;
     private previousMinutes: number;
     private previousSeconds: number;
+    private previousTimeMinutes: number;
+    private previousTimeSeconds: number;
 
+    isTimeSecondInRange(event) {
+        if (this.timeSeconds > 59) {
+            if (this.timeSeconds.toString().length > 2) {
+                var newValue = this.timeSeconds.toString().slice(0, 2);
+                event.target.value = newValue;
+                this.timeSeconds = parseInt(newValue);
+                this.previousTimeSeconds = this.timeSeconds;
+            } else {
+                event.target.value = this.previousTimeSeconds;
+                this.timeSeconds = this.previousTimeSeconds;
+            }
+        } else {
+            this.previousTimeSeconds = this.timeSeconds;
+        }
+    }
+    isTimeMinuteInRange(event) {
+        if (this.timeMinutes > 59) {
+            if (this.timeMinutes.toString().length > 2) {
+                var newValue = this.timeMinutes.toString().slice(0, 2);
+                event.target.value = newValue;
+                this.timeMinutes = parseInt(newValue);
+                this.previousTimeMinutes = this.timeMinutes;
+            } else {
+                event.target.value = this.previousTimeMinutes;
+                this.timeMinutes = this.previousTimeMinutes;
+            }
+        } else {
+            this.previousTimeMinutes = this.timeMinutes;
+        }
+    }
     isSecondInRange(event) {
         if (this.seconds > 59) {
             if (this.seconds.toString().length > 2) {
@@ -85,11 +119,45 @@ export class AppComponent {
         this.isSuggestedDistance = this.distances.indexOf(value) >= 0;
     }
 
+    calculatePaceTime() {
+        if (this.timeMinutes || this.timeSeconds) {
+            if (this.isSuggestedDistance) {
+                return this.calculateTimePace(this.calculateDistance());
+            } else if (/^\d{1,3}$/.test(this.distance)) {
+                const distanceInMiles =
+                    this.selectedDistanceUnit === 'miles'
+                        ? parseFloat(this.distance)
+                        : parseFloat(this.distance) * this.kilometerConversion;
+                return this.calculateTimePace(distanceInMiles);
+            } else {
+                return this.defaultPace;
+            }
+        } else {
+            return this.defaultPace;
+        }
+    }
+
+    calculatePaceDistance() {
+        if (this.hours || this.minutes || this.seconds) {
+            if (this.timeMinutes || this.timeSeconds) {
+                const time = this.calculateTime();
+                const paceTime =
+                    (this.timeMinutes > 0 ? this.timeMinutes * 60 : 0) +
+                    (this.timeSeconds > 0 ? this.timeSeconds : 0);
+                return (time / paceTime).toFixed(2);
+            } else {
+                return '0';
+            }
+        } else {
+            return '0';
+        }
+    }
+
     calculatePace() {
         if (this.hours || this.minutes || this.seconds) {
             if (this.isSuggestedDistance) {
                 return this.calculateDistancePace(this.calculateDistance());
-            } else if (/^\d{1,3}$/.test(this.distance)) {
+            } else if (/^\d{1,3}(.\d*)?$/.test(this.distance)) {
                 const distanceInMiles =
                     this.selectedDistanceUnit === 'miles'
                         ? parseFloat(this.distance)
@@ -103,10 +171,37 @@ export class AppComponent {
         }
     }
 
+    private calculateTimePace(distanceInMiles: number): string {
+        const paceTime =
+            (this.timeMinutes > 0 ? this.timeMinutes * 60 : 0) +
+            (this.timeSeconds > 0 ? this.timeSeconds : 0);
+        const time = paceTime * distanceInMiles;
+        let hourTime = Math.floor(time / 60 / 60);
+        let minuteTime = Math.floor((time - hourTime * 60 * 60) / 60);
+        let secondTime = Math.round(time - hourTime * 60 * 60 - minuteTime * 60);
+
+        if (secondTime >= 60) {
+            secondTime = 0;
+            minuteTime += 1;
+        }
+        if (minuteTime >= 60) {
+            minuteTime = 0;
+            hourTime += 1;
+        }
+
+        return (
+            this.formatTime(hourTime) +
+            ':' +
+            this.formatTime(minuteTime) +
+            ':' +
+            this.formatTime(secondTime)
+        );
+    }
+
     private calculateDistancePace(distanceInMiles: number): string {
         const pace = this.calculateTime() / distanceInMiles;
         let hourPace = Math.floor(pace / 60 / 60);
-        let minutePace = Math.floor((pace - hourPace * 60) / 60);
+        let minutePace = Math.floor((pace - hourPace * 60 * 60) / 60);
         let secondPace = Math.round(pace - hourPace * 60 * 60 - minutePace * 60);
 
         if (secondPace >= 60) {
